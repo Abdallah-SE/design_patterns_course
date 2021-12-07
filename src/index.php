d7860aae3d5642b0672f09636f057037d94543b2
Abdallah-SE <abdalla.a911@yahoo.com>
2021-12-07 22:55:09 +0200

    add employee class and require the autoloader

--- src/index.php
+++ src/index.php
@@ -1,2 +1,6 @@
 <?php
-namespace desgignpattern;
+//namespace desgignpattern;
+use desgignpattern\oop\Employee;
+require_once __DIR__.'/../vendor/autoload.php';
+$abdallah = new Employee("Abdallah mahmoud", 26);
+var_dump($abdallah);
